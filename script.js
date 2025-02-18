3db3b2dab55b53e71d10f37718c6f84850737c42
vinay pandey <abhi837688@gmail.com>
2025-02-18 13:21:29 +0530

    portfoilo

---
+++ script.js
@@ -0,0 +1,6 @@
+let typed = new Typed(".typing",{
+    strings:["","Frontend Developer", "Backend Devloper", "Full Stack Devloper", "web Desginer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
